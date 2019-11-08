9b189e960a50bdb19afc4e6332340e19fd6e430c
SuSu-Lin <1589372895@qq.com>
2019-11-08 22:13:01 +0800

    day5

---
+++ HuaWei-webapp/src/components/honour/honour-zujian/honour-two.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="swiper-container two">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/390.png" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/351.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/377.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/378.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/379.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/376.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/391.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/honour/img/392.jpg" alt="">
+        </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+</div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+export default {
+    name:"xhonour-two",
+    mounted () {
+    new Swiper ('.swiper-container', {
+    loop: true, // 循环模式选项
+    autoplay:true,
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  })        
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ HuaWei-webapp/src/components/huawei/huawei-zujian/huawei-one.vue
@@ -0,0 +1,53 @@
+<template>
+  <div class="swiper-container oneq">
+    <div class="swiper-wrapper">
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/300.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/378.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/379.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/380.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/381.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/382.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/376.jpg" alt="">
+        </div>
+        <div class="swiper-slide">
+            <img src="../../../../public/huawei/img/377.jpg" alt="">
+        </div>
+    </div>
+    <!-- 如果需要分页器 -->
+    <div class="swiper-pagination"></div>
+</div>
+</template>
+
+<script>
+import Swiper from 'swiper'
+export default {
+    name:"xhuawei-one",
+    mounted () {
+    new Swiper ('.swiper-container', {
+    loop: true, // 循环模式选项
+    autoplay:true,
+    // 如果需要分页器
+    pagination: {
+      el: '.swiper-pagination',
+    }
+  })        
+    }
+}
+</script>
+
+<style>
+
+</style>
